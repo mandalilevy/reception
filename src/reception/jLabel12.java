4b4a8e10a78893d205573ce71a633766d4daf0ca
levy <mandalilevy@gmail.com>
2021-02-24 14:16:31 +0300

    first commit

---
+++ src/reception/jLabel12.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reception;
+
+import java.awt.Cursor;
+
+/**
+ *
+ * @author LEVY
+ */
+class jLabel12 {
+
+    void setCursor(Cursor predefinedCursor) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
